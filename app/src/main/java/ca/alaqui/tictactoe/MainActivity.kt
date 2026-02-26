5abf4dc67af4c2ea5773a42b037597f9877d1a5e
andreilaqui <a.laqui837@mybvc.ca>
2026-02-25 22:18:41 -0700

    feat: add UI skeleton and  grid

--- app/src/main/java/ca/alaqui/tictactoe/MainActivity.kt
+++ app/src/main/java/ca/alaqui/tictactoe/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ca.alaqui.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +34,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TicTacToeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                TicTacToeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun TicTacToeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Tic-Tac-Toe",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Current Turn: X",
+            fontSize = 18.sp
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        TicTacToeBoard()
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(onClick = { /* TODO: reset later */ }) {
+            Text("Reset Game")
+        }
+    }
+}
+
+@Composable
+fun TicTacToeBoard() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f) // keeps it square
+            .border( width = 2.dp, color = MaterialTheme.colorScheme.onBackground)
+    ) {
+        repeat(3) { row ->
+            Row(modifier = Modifier.weight(1f)) {
+                repeat(3) { col ->
+                    TicTacToeCell(
+                        text = "",
+                        onClick = { /* will code later */ },
+                        modifier = Modifier.weight(1f)
                     )
                 }
             }
@@ -31,17 +96,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name World!!!",
+fun TicTacToeCell(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TicTacToeTheme {
-        Greeting("Android")
+            .fillMaxSize()
+            .border(width = 1.dp, color = MaterialTheme.colorScheme.onBackground),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
